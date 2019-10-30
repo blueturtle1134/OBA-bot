2b67b52a134666a26290c13a83efe5bef85b161b
blueturtle1134 <blueturtle1134@gmail.com>
2019-10-30 18:42:19 -0400

    Create Controls.java

---
+++ src/oba/gui/Controls.java
@@ -0,0 +1,33 @@
+package oba.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Controls {
+	
+	JFrame frame;
+	JPanel mainPanel;
+	
+	public Controls() {
+		frame = new JFrame("OBA Bot Controls");
+		frame.setSize(300, 400);
+		frame.setVisible(true);
+		
+		mainPanel = new JPanel();
+		frame.setContentPane(mainPanel);
+		
+		JButton closeButton = new JButton("Save and close");
+		closeButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+			
+		});
+	}
+}
