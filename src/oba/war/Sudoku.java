f672e6cf74ba69ecf13cb62d07a73399494e722a
blueturtle1134 <blueturtle1134@gmail.com>
2019-11-01 00:17:02 -0400

    Create Sudoku.java

---
+++ src/oba/war/Sudoku.java
@@ -0,0 +1,11 @@
+package oba.war;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Sudoku extends ListenerAdapter {
+	@Override
+	public void onMessageReceived(MessageReceivedEvent e) {
+		
+	}
+}
